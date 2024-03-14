b1bbf78aa067652b591d4e6865fcf1d78e9cb89e
TSnandini <161111297+TSnandini@users.noreply.github.com>
2024-03-14 20:09:25 +0530

    Add files via upload

---
+++ demoSRP.java
@@ -0,0 +1,21 @@
+package week6;
+import student.Student;
+import requestbook.requestbook;
+import issuebook.IssueBook;
+public class demoSRP 
+{
+	public static void main(String[] args) {
+        Student sony = new Student();
+        sony.setname("Sony Mathew");
+        sony.setregisterNO("119cs22035");
+        requestbook rb = new requestbook();
+        rb.setbookName("java programming");
+        rb.setduration(2);
+        rb.setStudent(sony);
+        rb.searchBook();
+        IssueBook ibOrder = new IssueBook(rb);
+        ibOrder.issubook();
+    }
+
+
+}
