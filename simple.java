2a6adfc4b007482374f410f9947a53f552d47c41
TSnandini <161111297+TSnandini@users.noreply.github.com>
2024-03-14 21:10:12 +0530

    Add files via upload

---
+++ simple.java
@@ -0,0 +1,21 @@
+package week1;
+
+public class simple 
+{
+	public static void main(String args[])
+	{
+		int result =0;
+		int num1 =4;
+		int num2 =2;
+		result = num1+num2;
+		System.out.println("the sum of "+num1+"and "+num2+" is "+result+".");
+		result = num1- num2;
+		System.out.println("the difference of "+num1+"and"+num2+"is "+result+".");
+		result=num1*num2;
+		System.out.println("the product of "+num1+"and"+num2+"is"+result+".");
+		result =num1*num2;
+		System.out.println("the reminder of "+num1+"and"+num2+ "is "+result+".");
+		result =num1%num2;
+		}
+
+}
